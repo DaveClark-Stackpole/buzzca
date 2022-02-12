044edd3c3787524434631e1b79e3cf167d9b3a7e
Dell Work Computer <dclark@stackpole.com>
2022-02-12 11:16:09 -0500

    test

---
+++ buzzca/views2.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+
+from datetime import datetime
+import MySQLdb
+import time
+from django.core.context_processors import csrf
+import smtplib
+from smtplib import SMTP
+
+
+# Call Main Login screen
+def test(request):
+	
+	return render(request, "test4.html")
