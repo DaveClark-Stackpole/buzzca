fb85ae1f65e81e68806d41558eb739eee87ebe1a
Dell Work Computer <dclark@stackpole.com>
2022-02-13 09:17:14 -0500

    f

--- buzzca/urls.py
+++ buzzca/urls.py
@@ -25,6 +25,8 @@ from django.conf.urls.static import static
 
 from buzzca.views2 import test
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     # url(r'^admin/',include(admin.site.urls)),
