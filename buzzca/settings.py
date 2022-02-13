9f5c3852c42c892f8f056ffb2c5dd4ecc5ba78ab
Dell Work Computer <dclark@stackpole.com>
2022-02-13 09:26:38 -0500

    j

--- buzzca/settings.py
+++ buzzca/settings.py
@@ -128,8 +128,11 @@ USE_TZ = True
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/buzzappc/public_html/static'
