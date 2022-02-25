047520fb2ad534f5c7eee9e2cf736ff4d6537c84
Dell Work Computer <dclark@stackpole.com>
2022-02-25 15:50:04 -0500

    update

--- buzzca/views2.py
+++ buzzca/views2.py
@@ -6,16 +6,19 @@ from datetime import datetime
 
 
 import os
+import time
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 
 # Call Main Login screen
 def test(request):
-	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-	# BASE_DIR = BASE_DIR + '\\static\\buzzca'
-	# t=4/0
-	dir = BASE_DIR
+	# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	# # BASE_DIR = BASE_DIR + '\\static\\buzzca'
+	# # t=4/0
+	# dir = BASE_DIR
 	# p=8/0
+	t=int(time.time())
+	request.session['TCURR'] = t
 
 	return render(request, "test4.html",)
